b73ad65b6b99dc52548f6d689f2134c660685068
kyle-lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-10-17 17:29:04 +0000

    pytrhyyh

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -7,5 +7,21 @@ This module is a Micro:bit MicroPython program
 from microbit import *
 import neopixel
 
+num_pixels = 12
 np = neopixel.NeoPixel(pin16, 4)
 
+while True:
+    if button_a.is_pressed():
+        np[2] = (0, 255, 0)
+        np.show()
+        sleep(1000)
+        np[2] = (0, 0, 0)
+        np[1] = (255, 255, 0)
+        np.show()
+        sleep(1000)
+        np[1] = (0, 0, 0)
+        np.show()
+        np[0] = (255, 0, 0)
+        np.show
+        sleep(1000)
+        np[0] = (0, 0, 0)
